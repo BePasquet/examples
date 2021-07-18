9eed530e0267ee25457e8fe1144deb37e0ed3525
bepasquet <plescanopasquet@gmail.com>
2021-07-18 08:04:57 +0700

    - routing set up

--- apps/ngrx-example/src/app/app.module.ts
+++ apps/ngrx-example/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     }),
     EffectsModule.forRoot([]),
     BrowserAnimationsModule,
+    AppRouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
