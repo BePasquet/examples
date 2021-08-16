9bce4b08b66e2c832fd745c917e4749ab6d98216
bepasquet <plescanopasquet@gmail.com>
2021-08-16 23:15:59 +0700

    - sw updates handling added

--- apps/ngrx-example/src/app/app.component.ts
+++ apps/ngrx-example/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Store } from '@ngrx/store';
+import { catchError, EMPTY, switchMap, take, tap } from 'rxjs';
 import { AuthenticationProvider } from './authentication';
+import { showMessage } from './layout';
 
 @Component({
   selector: 'examples-root',
@@ -7,7 +11,32 @@ import { AuthenticationProvider } from './authentication';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  constructor(private readonly authenticationProvider: AuthenticationProvider) {
+  private readonly reloadWhenSWUpdate$ = this.swUpdate.available.pipe(
+    switchMap(() => this.swUpdate.activateUpdate()),
+    tap(() => location.reload()),
+    catchError(() => EMPTY),
+    take(1)
+  );
+
+  private readonly showErrorWhenUnrecoverable$ =
+    this.swUpdate.unrecoverable.pipe(
+      tap(() =>
+        this.store.dispatch(
+          showMessage({
+            payload: `An error has ocurred please reload the page`,
+          })
+        )
+      ),
+      take(1)
+    );
+
+  constructor(
+    private readonly authenticationProvider: AuthenticationProvider,
+    private readonly swUpdate: SwUpdate,
+    private readonly store: Store
+  ) {
     this.authenticationProvider.checkAuthentication();
+    this.reloadWhenSWUpdate$.subscribe();
+    this.showErrorWhenUnrecoverable$.subscribe();
   }
 }
